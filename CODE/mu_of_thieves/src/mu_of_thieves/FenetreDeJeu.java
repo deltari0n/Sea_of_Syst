ff7b1bc9debb9f9d8038348c2cdc65704084c265
deltari0n <val106615@gmail.com>
2024-10-02 18:05:29 +0200

    [début code fentre2Jeu]

--- CODE/mu_of_thieves/src/mu_of_thieves/FenetreDeJeu.java
+++ CODE/mu_of_thieves/src/mu_of_thieves/FenetreDeJeu.java
@@ -4,10 +4,40 @@
  */
 package mu_of_thieves;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
  * @author vruche
  */
-public class FenetreDeJeu {
+public class FenetreDeJeu extends JFrame {
     
+    private BufferedImage framebuffer;
+    private Graphics2D contexte;
+    private JLabel jLabel1;
+    
+    public FenetreDeJeu() { 
+        // initialisation de la fenetre
+        this.setSize(380, 430);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.jLabel1 = new JLabel();
+        this.jLabel1.setPreferredSize(new java.awt.Dimension(380, 430));
+        this.setContentPane(this.jLabel1);
+        this.pack();
+    
+        // Creation du buffer pour l’affichage du jeu et recuperation du contexte graphique
+        this.framebuffer = new BufferedImage(this.jLabel1.getWidth(), this.jLabel1.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        this.jLabel1.setIcon(new ImageIcon(framebuffer));
+        this.contexte = this.framebuffer.createGraphics();
+    }
+    
+    public static void main(String[] args) {
+        FenetreDeJeu fenetre = new FenetreDeJeu();
+        fenetre.setVisible(true);
+    }
 }
