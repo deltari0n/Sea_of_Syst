446ad608b3479c368f7de6130c29338c13d4305f
deltari0n <val106615@gmail.com>
2024-10-02 16:14:09 +0200

    [add][all]

---
+++ src/main/java/com/mycompany/mu_of_thieves/Mu_of_thieves.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mu_of_thieves;
+
+/**
+ *
+ * @author vruche
+ */
+public class Mu_of_thieves {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
