bdb81a5e71c84d27d4b65200a46c72097383294e
deltari0n <val106615@gmail.com>
2024-10-22 17:41:43 +0200

    [start implement][boulet_2_canon]

--- CODE/mu_of_thieves/src/mu_of_thieves/Boulet_2_canon.java
+++ CODE/mu_of_thieves/src/mu_of_thieves/Boulet_2_canon.java
@@ -4,10 +4,27 @@
  */
 package mu_of_thieves;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author vruche
  */
 public class Boulet_2_canon extends Entite {
     
+    protected int x,y;
+    protected BufferedImage sprite;
+    protected Arraylist[] listePos;
+    
+    public Boulet_2_canon(){
+        try {
+            this.sprite = ImageIO.read(getClass().getResource("/ressources/Boulet.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(Joueur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
