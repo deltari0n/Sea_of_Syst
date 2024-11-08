45566f0073d3c0ce5504e14ce78582392c45342d
Val <val106615@gmail.com>
2024-11-08 15:22:45 +0100

    [start][map]

--- CODE/Sea_of_syst/src/mu_of_thieves/Map.java
+++ CODE/Sea_of_syst/src/mu_of_thieves/Map.java
@@ -4,10 +4,40 @@
  */
 package mu_of_thieves;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author vruche
  */
 public class Map {
+    
+    protected BufferedImage map;
+    
+    public Map(){
+        try {
+            this.map = ImageIO.read(getClass().getResource("/ressources/Boulet.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(Joueur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    //on va créer un tableau qui va convertir tout les pixel de la map en 1 et le fond en 0
+    
+    public void convPNGToBinaire(){
+        int n = map.getWidth();
+        int m = map.getHeight();
+        int[][] tab = new int[n][m];
+        for(int i=0;i<n;n++){
+            for(int j=0;j<m;j++){
+                System.out.println(map.getRGB(i, j));
+            }
+        }
+   }
     
 }
