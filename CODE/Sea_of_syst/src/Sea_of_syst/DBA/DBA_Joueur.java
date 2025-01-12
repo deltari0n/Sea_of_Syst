c9319efd1045802cf6685cde27479e295bbc3eb0
Zmourfatimaezzahra <zmourfatimaezzahrae@gmail.com>
2025-01-12 12:54:59 +0100

    [Correct]
    le probleme de mariadb-java-client-3.0.7.jar

--- CODE/Sea_of_syst/src/Sea_of_syst/DBA/DBA_Joueur.java
+++ CODE/Sea_of_syst/src/Sea_of_syst/DBA/DBA_Joueur.java
@@ -12,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import org.mariadb.jdbc.Statement;
+import java.sql.Statement;
 
 /**
  *
